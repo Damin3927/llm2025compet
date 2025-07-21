b61ffe6a2a79fb1b5e20f5f4c58f3e5345ea135d
Hiroaki Honda <d@damin.dev>
2025-07-21 17:19:06 +0900

    fix: Set max_tokens explicitly

--- tutorials/run_inference.py
+++ tutorials/run_inference.py
@@ -1,24 +1,31 @@
-from vllm import LLM
+from vllm import LLM, SamplingParams
 
 def main() -> None:
     # Initialize the LLM with the desired model
     # example: https://huggingface.co/collections/Qwen/qwen3-67dd247413f0e2e4f653967f
-    llm = LLM(model="Qwen/Qwen3-32B", tensor_parallel_size=4)
+    llm = LLM(model="Qwen/Qwen3-32B", tensor_parallel_size=4, trust_remote_code=True)
 
     # Run inference with a sample prompt
     # MATH-500 dataset example
     # https://huggingface.co/datasets/HuggingFaceH4/MATH-500/viewer/default/test?views%5B%5D=test
     # unique_id: test/intermediate_algebra/1994.json
-    prompt = """Define
+    prompt = r"""Define
 \[p = \sum_{k = 1}^\infty \frac{1}{k^2} \quad \text{and} \quad q = \sum_{k = 1}^\infty \frac{1}{k^3}.\]Find a way to write
 \[\sum_{j = 1}^\infty \sum_{k = 1}^\infty \frac{1}{(j + k)^3}\]in terms of $p$ and $q.$"""
 
-    response = llm.generate(prompt)
+    sampling_params = SamplingParams(
+        max_tokens=1024,
+        temperature=0.8,
+    )
+
+    response = llm.generate(prompt, sampling_params=sampling_params)
     
     # Print the response
     # Answer should be: p - q
     print("==" * 20)
     print(response[0].outputs[0].text)
+
+
 
 if __name__ == "__main__":
     main()
