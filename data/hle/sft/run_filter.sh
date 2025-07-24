7b356f8350d65c1197ddd0da69b3d7a8d1feb56b
ljy_local <juniorliu95@gmail.com>
2025-07-24 00:42:56 +0000

    set env

--- data/hle/sft/run_filter.sh
+++ data/hle/sft/run_filter.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=filter
 #SBATCH --partition=P02
 #SBATCH --nodes=1
-#SBATCH --nodelist=osk-gpu54
-#SBATCH --gres=gpu:8 # GPUが必要な場合
+#SBATCH --nodelist=osk-gpu56
+#SBATCH --gres=gpu:1 # GPUが必要な場合
 #SBATCH --time=5:00:00 # 実行に時間がかかる可能性を考慮して設定
 #SBATCH --output=/home/Competition2025/P02/P02U007/logs/%x-%j.out
 #SBATCH --error=/home/Competition2025/P02/P02U007/logs/%x-%j.err
@@ -24,10 +24,10 @@ nvidia-smi
 cd /home/Competition2025/P02/P02U007/llm2025compet/data/hle/sft
 
 CUDA_VISIBLE_DEVICES=0 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --start-from-percentage 0.0 --end-at-percentage 0.5
-CUDA_VISIBLE_DEVICES=1 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --start-from-percentage 0.5 --end-at-percentage 1.0
-CUDA_VISIBLE_DEVICES=2 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 2 --start-from-percentage 0.0 --end-at-percentage 0.5
-CUDA_VISIBLE_DEVICES=3 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 2 --start-from-percentage 0.5 --end-at-percentage 1.0
-CUDA_VISIBLE_DEVICES=4 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 3 --start-from-percentage 0.0 --end-at-percentage 0.5
-CUDA_VISIBLE_DEVICES=5 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 3 --start-from-percentage 0.5 --end-at-percentage 1.0
-CUDA_VISIBLE_DEVICES=6 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 4 --start-from-percentage 0.0 --end-at-percentage 0.5
-CUDA_VISIBLE_DEVICES=7 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 4 --start-from-percentage 0.5 --end-at-percentage 1.0
+# CUDA_VISIBLE_DEVICES=1 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --start-from-percentage 0.5 --end-at-percentage 1.0
+# CUDA_VISIBLE_DEVICES=2 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 2 --start-from-percentage 0.0 --end-at-percentage 0.5
+# CUDA_VISIBLE_DEVICES=3 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 2 --start-from-percentage 0.5 --end-at-percentage 1.0
+# CUDA_VISIBLE_DEVICES=4 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 3 --start-from-percentage 0.0 --end-at-percentage 0.5
+# CUDA_VISIBLE_DEVICES=5 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 3 --start-from-percentage 0.5 --end-at-percentage 1.0
+# CUDA_VISIBLE_DEVICES=6 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 4 --start-from-percentage 0.0 --end-at-percentage 0.5
+# CUDA_VISIBLE_DEVICES=7 python OpenMathReasoningFiltering.py --inference-model Qwen/Qwen3-32B --judgement-model Qwen/Qwen3-32B --run-index 4 --start-from-percentage 0.5 --end-at-percentage 1.0
