a2d82a90e436f4352cdc192f1b9def942456a945
ShotaKaji5207 <shotak.5207@gmail.com>
2025-08-08 12:20:47 +0900

    fix lora learning objects

--- training/open-r1/src/open_r1/grpo.py
+++ training/open-r1/src/open_r1/grpo.py
@@ -127,7 +127,6 @@ def main(script_args: GRPOScriptArguments, training_args: GRPOConfig, model_args
         task_type="CAUSAL_LM",
         target_modules=[
             "q_proj", "k_proj", "v_proj", "o_proj",
-            "gate_proj", "up_proj", "down_proj",
         ],
     )
 
